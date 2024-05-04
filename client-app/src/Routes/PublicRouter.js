b314438545e113196f32b6da18a54c7082bef259
PhamHoanVinh24 <phamhoanvinh24@gmail.com>
2024-05-04 10:22:46 +0700

    update 4/5

--- client-app/src/Routes/PublicRouter.js
+++ client-app/src/Routes/PublicRouter.js
@@ -9,7 +9,7 @@ import kid_banner from "../User/Components/Assets/banner_kids.png"
 import Shop from "../User/Pages/Shop";
 
 const PublicRouter = () => {
-    return ( 
+    return (
         <div>
             <BrowserRouter>
                 <Routes>
@@ -17,17 +17,20 @@ const PublicRouter = () => {
                         <Route index element={<Shop />} />
 
                         <Route path='iphone' element={<ShopCategory brand="iPhone" banner={men_banner} />} />
-                        <Route path='samsung' element={<ShopCategory brand="Samsung" banner={women_banner}/>} />
-                        <Route path='nokia' element={<ShopCategory brand="Nokia" banner={kid_banner}/>} />
-                        <Route path='iphone/:id' element={<Product/>}></Route>
+                        <Route path='samsung' element={<ShopCategory brand="Samsung" banner={women_banner} />} />
+                        <Route path='vivo' element={<ShopCategory brand="vivo" banner={women_banner} />} />
+                        <Route path='huawei' element={<ShopCategory brand="huawei" banner={women_banner} />} />
+                        <Route path='oppo' element={<ShopCategory brand="oppo" banner={women_banner} />} />
+                        <Route path='xiaomi' element={<ShopCategory brand="xiaomi" banner={women_banner} />} />
+                        <Route path='iphone/:id' element={<Product />}></Route>
                         <Route path='cart' element={<Cart />} />
                         <Route path='login' element={<Login />} />
                     </Route>
                 </Routes>
-              
+
             </BrowserRouter>
         </div>
-     );
+    );
 }
- 
+
 export default PublicRouter;
--- client-app/src/User/Components/Navbar/Navbar.jsx
+++ client-app/src/User/Components/Navbar/Navbar.jsx
@@ -7,6 +7,10 @@ import { ShopContext } from "../../Context/ShopContext";
 import nav_droppdown from "../Assets/dropdown_icon.png"
 import iphone_icon from "../Assets/iphone.png"
 import samsung_icon from "../Assets/samsung.png"
+import vivo_icon from "../Assets/vivo.png"
+import oppo_icon from "../Assets/oppo.png"
+import huawei_icon from "../Assets/huawei.png"
+import xiaomi_icon from "../Assets/xiaomi.png"
 const Navbar = () => {
     const [menu, setMenu] = useState();
     // const shopContext = useContext(ShopContext);
@@ -16,7 +20,7 @@ const Navbar = () => {
         menuRef.current.classList.toggle('nav-menu-visible')
         e.target.classList.toggle('open')
     }
-    return ( 
+    return (
         <>
             <div className="navbar">
                 <div className="nav-logo">
@@ -25,17 +29,36 @@ const Navbar = () => {
                 </div>
                 <img onClick={dropdown_toggle} src={nav_droppdown} alt="" className="nav-dropdown" />
                 <ul className="nav-menu" ref={menuRef}>
-
-                    <li onClick={() => {setMenu("iphone")}}>
-                        <Link to="/iphone" style={{textDecoration: "none"}}>
+                    <li onClick={() => { setMenu("iphone") }}>
+                        <Link to="/iphone" style={{ textDecoration: "none" }}>
                             <img clasname="icon" src={iphone_icon} alt="" />
-                        </Link>{menu==="iphone"?<hr/>:<></>}
+                        </Link>{menu === "iphone" ? <hr /> : <></>}
                     </li>
 
-                    <li onClick={() => {setMenu("samsung")}}>
-                        <Link to="/samsung" style={{textDecoration: "none"}}>
+                    <li onClick={() => { setMenu("samsung") }}>
+                        <Link to="/samsung" style={{ textDecoration: "none" }}>
                             <img classname="icon" src={samsung_icon} alt="" />
-                        </Link>{menu==="samsung"?<hr/>:<></>}
+                        </Link>{menu === "samsung" ? <hr /> : <></>}
+                    </li>
+                    <li onClick={() => { setMenu("vivo") }}>
+                        <Link to="/vivo" style={{ textDecoration: "none" }}>
+                            <img classname="icon" src={vivo_icon} alt="" />
+                        </Link>{menu === "vivo" ? <hr /> : <></>}
+                    </li>
+                    <li onClick={() => { setMenu("oppo") }}>
+                        <Link to="/oppo" style={{ textDecoration: "none" }}>
+                            <img classname="icon" src={oppo_icon} alt="" />
+                        </Link>{menu === "oppo" ? <hr /> : <></>}
+                    </li>
+                    <li onClick={() => { setMenu("huawei") }}>
+                        <Link to="/huawei" style={{ textDecoration: "none" }}>
+                            <img classname="icon" src={huawei_icon} alt="" />
+                        </Link>{menu === "huawei" ? <hr /> : <></>}
+                    </li>
+                    <li onClick={() => { setMenu("xiaomi") }}>
+                        <Link to="/xiaomi" style={{ textDecoration: "none" }}>
+                            <img classname="icon" src={xiaomi_icon} alt="" />
+                        </Link>{menu === "xiaomi" ? <hr /> : <></>}
                     </li>
 
                 </ul>
@@ -49,9 +72,9 @@ const Navbar = () => {
                     <div className="nav-cart-count">{0}</div>
                 </div>
             </div>
-            <Outlet/>
+            <Outlet />
         </>
-     );
+    );
 }
- 
+
 export default Navbar;
