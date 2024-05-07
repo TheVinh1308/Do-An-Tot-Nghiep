8fbd39e00a3e878541217928503798a8f446e489
PhamHoanVinh24 <phamhoanvinh24@gmail.com>
2024-05-07 16:47:56 +0700

    update 7/5

--- client-app/src/User/Components/Item/Item.jsx
+++ client-app/src/User/Components/Item/Item.jsx
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 import "./Item.css"
 const Item = (props) => {
 
-    return ( 
+    return (
         <>
+
             <div className="item">
                 <Link to={`${props.id}`}>
-                    <img onClick={window.scrollTo(0,0)} src={props.image} alt="" />
+                    <img onClick={window.scrollTo(0, 0)} src={props.image} alt="" />
                 </Link>
                 <p>{props.name}</p>
                 <div className="item-prices">
@@ -18,9 +19,9 @@ const Item = (props) => {
                     </div>
                 </div>
             </div>
-         
+
         </>
-     );
+    );
 }
- 
+
 export default Item;
--- client-app/src/User/Pages/ShopCategory.jsx
+++ client-app/src/User/Pages/ShopCategory.jsx
@@ -5,6 +5,11 @@ import Item from "../Components/Item/Item";
 import { ShopContext } from "../Context/ShopContext";
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
+import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { DropdownButton } from "react-bootstrap";
+
 const ShopCategory = (props) => {
     const { phones } = useContext(ShopContext)
     return (
@@ -17,7 +22,108 @@ const ShopCategory = (props) => {
                     Sort by <img src={dropdown_icon} alt="" />
                 </div>
             </div>
+            <div className="shopcategory-filter" >
+                <Dropdown    >
+                    <Dropdown.Toggle  >
+                        Chức năng
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu  >
+                        <Dropdown.Item name="choiGame">Chơi game</Dropdown.Item>
+                        <Dropdown.Item name="quayPhim">Quay phim</Dropdown.Item>
+                        <Dropdown.Item name="coBan">Chức năng cơ bản</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <Dropdown  >
+                    <Dropdown.Toggle className="shopcategory-fill" >
+                        Dung lượng
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu >
+                        <Dropdown.Item href="#/action-1" active>
+                            2 GB
+                        </Dropdown.Item>
+                        <Dropdown.Item href="#/action-1" active>
+                            3 GB
+                        </Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">4 GB</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">6 GB</Dropdown.Item>
+                        <Dropdown.Item href="#/action-4">8 GB</Dropdown.Item>
+                        <Dropdown.Item href="#/action-4">12 GB</Dropdown.Item>
+                        <Dropdown.Item href="#/action-4">16 GB</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown  >
+                    <Dropdown.Toggle className="shopcategory-fill" >
+                        Giá tiền
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu >
+                        <Dropdown.Item href="#/action-1" active>
+                            Action
+                        </Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown  >
+                    <Dropdown.Toggle className="shopcategory-fill" >
+                        Lượng Pin
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu >
+                        <Dropdown.Item href="#/action-1" active>
+                            Action
+                        </Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown  >
+                    <Dropdown.Toggle className="shopcategory-fill" >
+                        Kích thước
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu >
+                        <Dropdown.Item href="#/action-1" active>
+                            Action
+                        </Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown  >
+                    <Dropdown.Toggle className="shopcategory-fill" >
+                        Chip xử lý
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu >
+                        <Dropdown.Item href="#/action-1" active>
+                            Action
+                        </Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+            <div className="shopcategory-filtered">
+                <h4>Các mục đã lọc</h4>
+                <hr />
+                <button className="shopcategory-fill">Chơi game <button className="cancel-fill">x</button></button>
+                <button className="shopcategory-fill">Chơi game <button className="cancel-fill">x</button></button>
+                <button className="shopcategory-fill">Chơi game <button className="cancel-fill">x</button></button>
+            </div>
             <div className="shopcategory-products">
+
                 {phones.map((item, index) => (
                     item !== null ?
                         props.brand === item.modPhone.brand.name
